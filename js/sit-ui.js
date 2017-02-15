b12b2de45cb0a30dc2b30c1f576fce8862a4165f
Tendance Web <nchemil@gmail.com>
2017-02-16 00:17:49 +0100

    menu

--- js/sit-ui.js
+++ js/sit-ui.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     $('.open_GRD').click(function() {
       $('#GRD').toggleClass('GRD_opened');
     });
+
+    $('.open_GRD').hover(function() {
+      //
+    }, function() {
+      //
+    });
     $('.bt_close').click(function() {
       $('#GRD').toggleClass('GRD_opened');
     });
