f21dafdce80e3014f9b8c814bdd6ee27beec131f
Tendance Web <nchemil@gmail.com>
2017-03-11 19:30:43 +0100

    MISE EN LIGNE DES PAGES

--- js/sit-ui.js
+++ js/sit-ui.js
@@ -1,13 +1,59 @@
 $(document).ready(function() {
+
+function ClassScroll() {
+  $(window).scroll(function() {
+    $('h1.card-title').addClass('disappear');
+    $('.page .fixed').addClass('top');
+    $('li:first-child .image').addClass('top');
+  });
+}
+
+function WindowTest() {
+
+        // Test de la largeur de la fenêtre
+        var windowWidth= $(window).width();
+
+        if(windowWidth < 520){
+          $('h1.card-title').addClass('disappear');
+          $('.page .fixed').addClass('top');
+          $('li:first-child .image').addClass('top');
+        }
+
+}
+
+
+  // Ouverture section GRD
     $('.open_GRD').click(function() {
-      $('#GRD').toggleClass('GRD_opened');
-      $('.bt_close').addClass('bt_close--show');
+    $('nav h1.card-title').next().append().load( "../bloc_html/GRD.html",function() {
+
+      // Chargement des fonctions de test
+      ClassScroll();
+      WindowTest();
+
+              // Rechargement du menu de la page d'accueil
+              $('.bt_close').click(function() {
+                  $('nav h1.card-title').next().append().load( "../bloc_html/menu__content.html");
+                  location.reload();
+              });
+            });
     });
 
-    $('.bt_close').click(function() {
-      $(this).removeClass('bt_close--show');
-      //alert('bouton cliqué');
-      $('#GRD').toggleClass('GRD_opened');
-    });
+            // Ouverture section GE
+              $('.open_GE').click(function() {
+              $('nav h1.card-title').next().append().load( "../bloc_html/GE.html",function() {
+
+
+                // Chargement des fonctions de test
+                ClassScroll();
+                WindowTest();
+                // Rechargement du menu de la page accueil
+                        $('.bt_close').click(function() {
+                            $('nav h1.card-title').next().append().load( "../bloc_html/menu__content.html");
+                            location.reload();
+                        });
+
+              });
+              });
+
 
 });
