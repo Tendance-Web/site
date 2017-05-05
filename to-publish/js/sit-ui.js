1e42df3154111ca6993be5bd91dd7617fba9189a
Nassim <nchemil@gmail.com>
2017-05-05 15:53:30 +0200

    menu home pour tw

---
+++ to-publish/js/sit-ui.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    $('#pageContenu h1.card-title').addClass('disappear');
+    $('#pageContenu .page .fixed').addClass('top');
+    $('li:first-child .image').addClass('top');
+  });
+
+    $('#pageContenu .bt_close').click(function() {
+      location.href = '/';
+    })
+
+    function ClassScroll() {
+      $(window).scroll(function() {
+        $('h1.card-title.on-scroll-disappear').addClass('disappear');
+        $('.page .fixed').addClass('top');
+        $('li:first-child .image').addClass('top');
+      });
+    }
+    function WindowTest() {
+
+            // Test de la largeur de la fenêtre
+            var windowWidth= $(window).width();
+
+            if(windowWidth < 520){
+              $('h1.card-title').addClass('disappear');
+              $('.page .fixed').addClass('top');
+              $('li:first-child .image').addClass('top');
+            }
+
+    }
+
+  // Ouverture section GRD
+    $('.open_GRD').click(function() {
+    $('nav h1.card-title').next().append().load( "../bloc_html/GRD.html",function() {
+
+      // Chargement des fonctions de test
+      ClassScroll();
+      WindowTest();
+
+              // Rechargement du menu de la page d'accueil
+              $('.bt_close').click(function() {
+
+                  $('nav h1.card-title').next().append().load( "../bloc_html/menu__content.html");
+                  location.reload();
+              });
+            });
+    });
+
+// Ouverture section GE
+  $('.open_GE').click(function() {
+  $('nav h1.card-title').next().append().load( "../bloc_html/GE.html",function() {
+    // Chargement des fonctions de test
+    ClassScroll();
+    WindowTest();
+    // Rechargement du menu de la page accueil
+            $('.bt_close').click(function() {
+                $('nav h1.card-title').next().append().load( "../bloc_html/menu__content.html");
+                location.reload();
+            });
+
+  });
+  });
+
+  // Ouverture section GE
+    $('.open_VITALITY').click(function() {
+    $('nav h1.card-title').next().append().load( "../bloc_html/VITALITY.html",function() {
+      // Chargement des fonctions de test
+      ClassScroll();
+      WindowTest();
+      // Rechargement du menu de la page accueil
+              $('.bt_close').click(function() {
+                  $('nav h1.card-title').next().append().load( "../bloc_html/menu__content.html");
+                  location.reload();
+              });
+
+    });
+    });
+
+
+
+});
---
+++ to-publish/js/ui.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+        $('#pageContenu .bt_close').click(function() {
+          location.href = '../';
+        });
+});
+
+$(window).load(function(){
+  $(".to-fix").sticky({ topSpacing: 0 });
+});
