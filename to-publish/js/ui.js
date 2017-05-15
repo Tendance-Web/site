eb18d2bcdfb6c1a72b59f42417c14c727d2f50f8
Nassim <nchemil@gmail.com>
2017-05-15 17:55:38 +0200

    maj design black

--- to-publish/js/ui.js
+++ to-publish/js/ui.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
         $('#pageContenu .bt_close').click(function() {
           location.href = '../';
         });
+        $('#menu__content').append($("footer"));
 });
 
 $(window).load(function(){
